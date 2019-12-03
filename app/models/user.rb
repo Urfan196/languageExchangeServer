3f1bdda8d3800cceb4db15e9a9f67d76916171db
github username <github email address>
2019-12-03 14:54:12 -0800

    implemented auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  # has_secure_password
+  has_secure_password
     
   has_many :fluencies
   has_many :languages, through: :fluencies
@@ -8,26 +8,27 @@ class User < ApplicationRecord
   has_many :teacher_appointment, foreign_key: :student_id, class_name: "Appointment"
   has_many :teachers, through: :teacher_appointment, source: :teacher
   has_many :availabilities
+  # validates :username, uniqueness: { case_sensitive: false }
+
+
   
-  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
-  
-  PASSWORD_FORMAT = /\A
-  (?=.{8,})          # Must contain 8 or more characters
-  (?=.*\d)           # Must contain a digit
-  (?=.*[a-z])        # Must contain a lower case character
-  (?=.*[A-Z])        # Must contain an upper case character
-  (?=.*[[:^alnum:]]) # Must contain a symbol
-  /x
-  validates :password, 
-  presence: true, 
-  length: { in: 8..20 }, 
-  format: { with: PASSWORD_FORMAT }, 
-  confirmation: true, 
-  on: :create 
-  validates :password, 
-  allow_nil: true, 
-  length: { in: 8..20 }, 
-  format: { with: PASSWORD_FORMAT }, 
-  confirmation: true, 
-  on: :update
+  # PASSWORD_FORMAT = /\A
+  # (?=.{8,})          # Must contain 8 or more characters
+  # (?=.*\d)           # Must contain a digit
+  # (?=.*[a-z])        # Must contain a lower case character
+  # (?=.*[A-Z])        # Must contain an upper case character
+  # (?=.*[[:^alnum:]]) # Must contain a symbol
+  # /x
+  # validates :password, 
+  # presence: true, 
+  # length: { in: 8..20 }, 
+  # format: { with: PASSWORD_FORMAT }, 
+  # confirmation: true, 
+  # on: :create 
+  # validates :password, 
+  # allow_nil: true, 
+  # length: { in: 8..20 }, 
+  # format: { with: PASSWORD_FORMAT }, 
+  # confirmation: true, 
+  # on: :update
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,75 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+languages = ["Amharic",	
+    "Arabic",	
+    "Basque",	
+    "Bengali",	
+    "English (UK)",	
+    "Portuguese",
+    "Bulgarian",	
+    "Catalan",
+    "Cherokee",	
+    "Croatian",
+    "Czech",
+    "Danish",
+    "Dutch",
+    "English (US)",
+    "Estonian",
+    "Filipino",
+    "Finnish",
+    "French",
+    "German",
+    "Greek",
+    "Gujarati",
+    "Hebrew",
+    "Hindi",	
+    "Hungarian",	
+    "Icelandic",	
+    "Indonesian",	
+    "Italian",	
+    "Japanese",	
+    "Kannada",	
+    "Korean",	
+    "Latvian",
+    "Lithuanian",	
+    "Malay",	
+    "Malayalam",	
+    "Marathi",	
+    "Norwegian",	
+    "Polish",	
+    "Portuguese (Portugal)",
+    "Romanian",
+    "Russian",	
+    "Serbian",
+    "Chinese (PRC)",
+    "Slovak",	
+    "Slovenian",	
+    "Spanish",	
+    "Swahili",
+    "Swedish",	
+    "Tamil",	
+    "Telugu",
+    "Thai",	
+    "Chinese (Taiwan)",	
+    "Turkish",	
+    "Urdu",
+    "Ukrainian",
+    "Vietnamese",	
+    "Welsh"
+]
+
+
+languages.each do |language|
+    Language.create(name: language)
+end
+
+user_1 = User.create(first_name: "John", last_name: "Doe", birthday: "21/01/1990", password_digest: "1234@T123t", username: "johndoe@fakeemail.com" )
+user_2 = User.create(first_name: "Jane", last_name: "Doe", birthday: "13/05/1989", password_digest: "hello456ft", username: "janedoe@fakeemail.com" )
+user_3 = User.create(first_name: "Carl", last_name: "Allen", birthday: "30/11/1947", password_digest: "idc147ll", username: "carlallen@fakeemail.com" )
+
+
+
+beginner = Fluency.create(level: "Beginner", user_id: rand(1..3), language_id: rand(0..20))
+elementary = Fluency.create(level: "Elementary", user_id: rand(1..3), language_id: rand(0..20))
+intermediate = Fluency.create(level: "Intermediate", user_id: rand(1..3), language_id: rand(0..20))
+advanced = Fluency.create(level: "Advanced", user_id: rand(1..3), language_id: rand(0..20))
+expert = Fluency.create(level: "Expert", user_id: rand(1..3), language_id: rand(0..20))
+native = Fluency.create(level: "Native", user_id: rand(1..3), language_id: rand(0..20))
